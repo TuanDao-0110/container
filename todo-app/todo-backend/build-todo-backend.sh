11047ee2ce5fec8354a19e0e80203db1dd21c825
TuanDao-0110 <rickytuan1991@gmail.com>
2023-12-08 23:58:07 +0200

    update connect between mongo and backend in docker compose

--- todo-app/todo-backend/build-todo-backend.sh
+++ todo-app/todo-backend/build-todo-backend.sh
@@ -1,3 +1,2 @@
 docker build -t todo-backend-image .
 docker run --rm -it --name todo-container-backend -p 8000:3000 -v /Users/nhattuandao/bclearn/part12-containers-applications/todo-app/todo-backend:/usr/src/app todo-backend-image
-
